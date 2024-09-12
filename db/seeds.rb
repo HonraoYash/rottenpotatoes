92474d510ce1df325b58647230f2fcffb471e1ce
Yash Honrao <yashhonrao2024@gmail.com>
2024-09-11 20:08:52 -0500

    second commit without heroku upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+    :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+    :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+    :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+    :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'}
+  {:title => 'Fast and Furious', :rating => 'PG-13', :release_date => '16-Jul-2010'},
+  {:title => 'Godfather', :rating => 'R', :release_date => '31-Mar-1999'},
+  {:title => 'Titanic', :rating => 'PG-13', :release_date => '07-Nov-2014'}
+]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
